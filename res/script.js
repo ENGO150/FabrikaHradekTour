b895548bda61f0693fbaee88f4694386c2d1913d
ENGO150 <v.smejkal06@gmail.com>
2023-05-16 15:13:10 +0200

    drawing test image into canvas (full)
    idk if I won't remake this somehow differently

--- res/script.js
+++ res/script.js
@@ -1,4 +1,18 @@
 function load_panorama()
 {
-    document.getElementById("panorama").style.backgroundImage = "./img/test.jpg" //NOT WORKING
+    console.log(window.width);
+    var ctx = document.getElementById('panorama').getContext('2d');
+    var img = new Image();
+    var width_buffer;
+
+    img.onload = function()
+    {
+        width_buffer = 5000;
+
+        ctx.canvas.width=width_buffer;
+        ctx.canvas.height=width_buffer / 2;
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, width_buffer, width_buffer / 2);
+    };
+
+    img.src = './res/img/test.jpg';
 }
