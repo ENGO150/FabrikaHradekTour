a7d3f967a0d42036a60f18fd11169fd392146960
ENGO150 <v.smejkal06@gmail.com>
2023-06-02 10:44:54 +0200

    added authors clickable shit

---
+++ res/autori/script.js
@@ -0,0 +1,8 @@
+window.onload = function()
+{
+    document.getElementById("sebedestrukce").onclick = (e) = function()
+    {
+        document.getElementById("sebedestrukce").style.color = "#800500";
+        document.getElementById("sebedestrukce").style.fontWeight = "bold";
+    }
+}
